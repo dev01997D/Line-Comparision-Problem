51581ec0835dd87ddd516a8f0122e4808877bf77
dev01997D <devnandan01997@gmail.com>
2020-09-25 18:11:02 +0530

    Added: Welcome message added

---
+++ LineComparision.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.linecomparisionproblem;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		//Welcome message
+		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+	}
+}
