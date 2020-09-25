edfbe17f30ac3c5dee3110371fe5c07561dc56d3
dev01997D <devnandan01997@gmail.com>
2020-09-25 20:24:21 +0530

    Modified: Comparision of length of two lines by compareTo method functionality

--- LineComparision.java
+++ LineComparision.java
@@ -6,14 +6,14 @@ public class LineComparision {
 	private static DecimalFormat df=new DecimalFormat("0.00");
 
 	public static void main(String[] args) {
-		//Welcome message
-		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+	    //Welcome message
+	    System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
 		
-		//Variables
-		double length1, length2;
-		boolean compareResult;
-		int x1=(int) Math.floor(Math.random()*100);
-		int x2=(int) Math.floor(Math.random()*100);
+	    //Variables
+	    double length1, length2;
+	    int compareValue;
+	    int x1=(int) Math.floor(Math.random()*100);
+	    int x2=(int) Math.floor(Math.random()*100);
 	    int y1=(int) Math.floor(Math.random()*100);
 	    int y2=(int) Math.floor(Math.random()*100);
 	    int x3=(int) Math.floor(Math.random()*100);
@@ -25,15 +25,22 @@ public class LineComparision {
 	    length1=Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
 	    length2=Math.sqrt(Math.pow((x4-x3), 2) + Math.pow((y4-y3), 2));
 	    
-	    //Comparision by equals method
-	    compareResult=df.format(length1).equals(df.format(length2));
+	    //Wrapper class
+            Double obj1 = new Double(df.format(length1)); 
+            Double obj2 = new Double(df.format(length2));
+        
+           //Comparision by compareTo method
+            compareValue = obj1.compareTo(obj2); 
 	    
 	    System.out.println("Length of line 1 is : "+df.format(length1) +"\tline 2 is : "+df.format(length2));
-	    if(compareResult) {
+	    if(compareValue==0) {
 	    	System.out.println("Both the lines are equal");
 	    }
+	    else if(compareValue<0){
+	    	System.out.println("Line 2 is greater than line 1");
+	    }
 	    else {
-	    	System.out.println("Both the lines are not equal");
+	    	System.out.println("Line 1 is greater than line 2");
 	    }
 	}
 }
