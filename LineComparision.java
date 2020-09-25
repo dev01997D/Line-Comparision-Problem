a374b10cae6fcb5f7b19a8ad2ea3a51b3db1db01
dev01997D <devnandan01997@gmail.com>
2020-09-25 20:04:46 +0530

    Added: Comparision of length of two lines by equal method functionality added

--- LineComparision.java
+++ LineComparision.java
@@ -10,15 +10,30 @@ public class LineComparision {
 		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
 		
 		//Variables
-		double length;
+		double length1, length2;
+		boolean compareResult;
 		int x1=(int) Math.floor(Math.random()*100);
 		int x2=(int) Math.floor(Math.random()*100);
 	    int y1=(int) Math.floor(Math.random()*100);
 	    int y2=(int) Math.floor(Math.random()*100);
+	    int x3=(int) Math.floor(Math.random()*100);
+	    int x4=(int) Math.floor(Math.random()*100);
+	    int y3=(int) Math.floor(Math.random()*100);
+	    int y4=(int) Math.floor(Math.random()*100);
 	    
 	    //Length computation
-	    length=Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+	    length1=Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+	    length2=Math.sqrt(Math.pow((x4-x3), 2) + Math.pow((y4-y3), 2));
 	    
-	    System.out.println("Length of a line is :"+df.format(length));
+	    //Comparision by equals method
+	    compareResult=df.format(length1).equals(df.format(length2));
+	    
+	    System.out.println("Length of line 1 is : "+df.format(length1) +"\tline 2 is : "+df.format(length2));
+	    if(compareResult) {
+	    	System.out.println("Both the lines are equal");
+	    }
+	    else {
+	    	System.out.println("Both the lines are not equal");
+	    }
 	}
 }
